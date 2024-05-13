9c051b5fec94588dfd37471466646f92b7938cd8
paulliwali <pauldeng93@gmail.com>
2024-05-13 13:15:46 -0700

    Added aaronson prediction algorithm and accuracy value

--- README.md
+++ README.md
@@ -1,27 +1,42 @@
-Adapting Aaronson's Oracle to predict a starting pitcher's next baseball pitch.
+Adapting [Aaronson's Oracle](https://github.com/elsehow/aaronson-oracle/blob/master/README.md) to predict a starting pitcher's next baseball pitch.
 
+The idea is to use a simple algorithm of remembering the most probable next thing given the past n combinations to make a *surprisingly* good prediction.
 
 ## Pseudo-code
 
-1. Fetch pitch by pitch log
+1. Fetch a starting pitchers game's pitch by pitch log using `pybaseball`
+    * Gets Statscast data which categorizes each pitch type
 1. Initialize a dictionary that will store a n-gram combination as the key and the number of times each pitch type that follows this n-gram combination as dict of values
-1. Record the rolling n-gram combination as you loop through the pitch log
-1. Look up the highest pitch type value, return that or default to one by chance if there are no stored values
+1. Record the rolling n-gram combination as you iterate through the pitch log
 1. Update the dictionary with the actual next pitch
+1. Look up the highest pitch type value, return that or default to one by chance if there are no stored values as the prediction
 
 ## Statscast Pitch Types
 
-CH: changeup
-CU: curveball
-FC: cutter 
-EP: eephus
-FO: forkball 
-FF: four-seam fastball 
-KN: knuckleball 
-KC: knuck curve
-SC: screwball
-SI: sinker
-SL: slider
-SV: slurve
-FS: splitter 
-ST: sweeper
+- CH: changeup
+- CU: curveball
+- FC: cutter 
+- EP: eephus
+- FO: forkball 
+- FF: four-seam fastball 
+- KN: knuckleball 
+- KC: knuck curve
+- SC: screwball
+- SI: sinker
+- SL: slider
+- SV: slurve
+- FS: splitter 
+- ST: sweeper
+
+Convert to three categories of "fast", "breaking", "off-speed". 
+
+## Run
+
+`python app.py` and navigate to http://127.0.0.1:5000
+
+
+## TODO
+ 
+- [ ] How does the naive prediction do, guess "fast" for all
+- [ ] How does random guessing with weighted percentages do
+- [ ] Calculate the rolling prediction 
--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-from flask import Flask, render_template, request, jsonify
-from pybaseball import cache, statcast_pitcher, playerid_lookup
+import json
+from typing import List
+
+import pandas as pd
+from flask import Flask, jsonify, render_template, request
+from pybaseball import cache, playerid_lookup, statcast_pitcher
+
+DEFAULT_PITCH_VALUE = "fast"
+PITCH_GRAM_SIZE = 3
 
 cache.enable()
 app = Flask(__name__)
 
-
 # List of baseball pitchers
-players = ["Logan Webb", "Sandy Alcantara", "Alek Manoah"]
+players = [
+    "Logan Webb",
+    "Corbin Burnes",
+    "Zac Gallen",
+    "Gerrit Cole",
+    "Blake Snell",
+    "Zack Wheeler",
+    "Kodai Senga",
+]
 
 
 def get_player_id(player_name: str) -> int:
+    """Get the Statscast player ID given the player name"""
+
     selected_player_last_name = player_name.split(" ")[1]
     selected_player_first_name = player_name.split(" ")[0]
     print(f"You selected: {selected_player_last_name}, {selected_player_first_name}")
@@ -20,6 +36,71 @@ def get_player_id(player_name: str) -> int:
     )
 
     return playerid_lookup_df["key_mlbam"].iloc[0]
+
+
+def map_pitch_type(game_stats_df: pd.DataFrame) -> pd.DataFrame:
+    """Map the Statscast pitch type to a simplified version"""
+    with open("data/pitch_map.json", "r") as f:
+        pitch_map = json.load(f)
+
+    game_stats_df["pitch_type_simplified"] = game_stats_df.loc[:, "pitch_type"].replace(
+        pitch_map
+    )
+
+    return game_stats_df
+
+
+def update_model(model: dict, pitch_gram: str, next_pitch: str) -> None:
+    """Update the model by either incrementing the value of a existing pitch_gram or
+    initializing it with a value of 1
+    """
+    if pitch_gram in model:
+        # Update the value
+        model[pitch_gram][next_pitch] = model[pitch_gram].get(next_pitch, 0) + 1
+    else:
+        # Initialize the value
+        model[pitch_gram] = {next_pitch: 1}
+
+
+def make_prediction(model: dict, pitch_gram: str) -> str:
+    """Make a prediction with the highest value for the next possible pitch
+    or use the default value if it doesn't exist"""
+    if pitch_gram in model:
+        next_possible_pitch = model[pitch_gram]
+        return max(next_possible_pitch, key=next_possible_pitch.get)
+
+    else:
+        return DEFAULT_PITCH_VALUE
+
+
+def create_valid_pitch_gram(pitches: List[str], pitch_gram_size: int) -> str:
+    """Create a valid pitch gram"""
+    if len(pitches) == pitch_gram_size:
+        return "".join(p for p in pitches)
+
+
+def predict_pitch_type(game_stats_df: pd.DataFrame, gram_size: int) -> list:
+    """Iterate through the pitches and make predictions while updating the model"""
+    predicted_pitch = []
+    model = {}
+
+    for i in range(len(game_stats_df)):
+        past_pitches = (
+            game_stats_df["pitch_type_simplified"].iloc[i - gram_size : i].to_list()
+        )
+
+        next_pitch = game_stats_df["pitch_type_simplified"].iloc[i]
+        pitch_gram = create_valid_pitch_gram(
+            pitches=past_pitches, pitch_gram_size=PITCH_GRAM_SIZE
+        )
+
+        # Make the prediction
+        predicted_pitch.append(make_prediction(model=model, pitch_gram=pitch_gram))
+
+        # Update the model
+        update_model(model=model, pitch_gram=pitch_gram, next_pitch=next_pitch)
+
+    return predicted_pitch
 
 
 @app.route("/")
@@ -34,8 +115,8 @@ def get_player_stats():
 
     # Get the statcast pitcher data
     selected_player_df = statcast_pitcher(
-        start_dt="2024-03-01",
-        end_dt="2024-05-01",
+        start_dt="2023-04-01",
+        end_dt="2024-01-01",
         player_id=selected_player_id,
     )
 
@@ -64,7 +145,26 @@ def get_game_stats():
     )
     print(f"Finished fetching {len(game_stats_df)} rows of single-game data")
 
-    return jsonify({"game_stats": game_stats_df.to_json(orient="records")})
+    # Map Statscast pitch type to simpler version
+    game_stats_df = map_pitch_type(game_stats_df=game_stats_df)
+
+    # Predict
+    game_stats_df["pitch_type_predicted"] = predict_pitch_type(
+        game_stats_df=game_stats_df, gram_size=PITCH_GRAM_SIZE
+    )
+
+    game_stats_df["is_correct"] = (
+        game_stats_df["pitch_type_predicted"] == game_stats_df["pitch_type_simplified"]
+    )
+
+    game_accuracy = round(sum(game_stats_df["is_correct"]) / len(game_stats_df), 4)
+
+    return jsonify(
+        {
+            "game_stats": game_stats_df.to_json(orient="records"),
+            "game_accuracy": game_accuracy,
+        }
+    )
 
 
 if __name__ == "__main__":

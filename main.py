8eaa75d804241daa78bc14ee81f67a7285efbddb
paulliwali <pauldeng93@gmail.com>
2024-09-20 22:00:41 -0700

    Adds required files for deployment on railway

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import json
 from typing import List
 
@@ -205,4 +206,5 @@ def get_game_stats():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
